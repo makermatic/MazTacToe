eebe3ac5eb424f8070afea0764c6984cba400083
makermatic <44180512+makermatic@users.noreply.github.com>
2021-08-30 16:07:21 -0400

    Added random colors for each user

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,9 @@
 let bg;
 
+// Color Vars
+let r1, g1, b1;
+let r2, g2, b2;
+
 function preload() {
     bg = loadImage('assets/Maz.png');
 }
@@ -10,12 +14,20 @@ function setup() {
 
     socket = io.connect('http://localhost:3000');
     socket.on('mouse', newDrawing);
+
+    r1 = random(255);
+    g1 = random(255);
+    b1 = random(255);
+
+    r2 = random(255);
+    g2 = random(255);
+    b2 = random(255);
 }
 
 function newDrawing(data) {
     // Draw a line
     noStroke();
-    fill("#0088ff");
+    fill(r2, g2, b2);
     ellipse(data.x, data.y, 7, 7);
 }
 
@@ -31,6 +43,6 @@ function mouseDragged() {
 
     // Draw a line
     noStroke();
-    fill("#db0f00");
+    fill(r1, g1, b1);
     ellipse(mouseX, mouseY, 7, 7);
 }
