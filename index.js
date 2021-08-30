fa4635b6b067f6adcf9bed37ee64d9fe5664d33f
makermatic <44180512+makermatic@users.noreply.github.com>
2021-08-30 16:52:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen('http://45.77.216.125:3000/');
+var server = app.listen(process.env.PORT || 3000);
 
 app.use(express.static('public'));
 
