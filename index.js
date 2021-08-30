26291206acfb9bf79d05ba234b1f8e8a0fab7d56
makermatic <44180512+makermatic@users.noreply.github.com>
2021-08-30 16:20:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen('https://maz-tac-toe.herokuapp.com/', { secure: true });
+var server = app.listen(process.env.PORT);
 
 app.use(express.static('public'));
 
